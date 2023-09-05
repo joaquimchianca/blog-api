a1160c2cad962490809a99ef3d14ba38df0e90c7
joaquimchianca <joaquimchianca@gmail.com>
2023-09-04 23:39:00 -0300

    first commit

---
+++ src/test/java/br/joaquim/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.joaquim.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

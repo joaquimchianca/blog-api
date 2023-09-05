8d2952bf6637b57aead4b8482313fc9e232e27f4
joaquimchianca <joaquimchianca@gmail.com>
2023-09-04 23:51:23 -0300

    feat: post jpa entity

---
+++ src/main/java/br/joaquim/blog/model/Post.java
@@ -0,0 +1,24 @@
+package br.joaquim.blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table(name = "tb_post",uniqueConstraints = {@UniqueConstraint(columnNames = "title")})
+public class Post {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+
+}
